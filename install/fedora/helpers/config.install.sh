2141cc4f43fbda31ae1d9a46ea5f45e06bcbc692
Jacob Ranson <jacobrileighranson@gmail.com>
2021-03-11 16:56:12 -0500

    Add kitty startup session

--- install/fedora/helpers/config.install.sh
+++ install/fedora/helpers/config.install.sh
@@ -6,6 +6,7 @@ echo
 
 ln -s ~/.config/fish/config.fish ../../config/config.fish
 ln -s ~/.config/kitty/kitty.conf ../../config/kitty.conf
+ln -s ~/.config/kitty/startup.session ../../config/startup.session
 ln -s ~/.config/micro/settings.json ../../config/micro.json
 ln -s ~/.gitconfig ../../config/gitconfig
 
