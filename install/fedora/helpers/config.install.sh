0c7741fae2fb83a8b0476f57e02451c3588d8455
Jacob Ranson <jacobrileighranson@gmail.com>
2021-03-11 16:34:14 -0500

    Add config install script

--- install/fedora/helpers/config.install.sh
+++ install/fedora/helpers/config.install.sh
@@ -4,6 +4,11 @@ echo
 echo "=== Start 'config.install.sh' ==="
 echo
 
+ln -s ~/.config/fish/config.fish ../../config/config.fish
+ln -s ~/.config/kitty/kitty.conf ../../config/kitty.conf
+ln -s ~/.config/micro/settings.json ../../config/micro.json
+ln -s ~/.gitconfig ../../config/gitconfig
+
 echo
 echo "=== End 'config.install.sh' ==="
 echo
