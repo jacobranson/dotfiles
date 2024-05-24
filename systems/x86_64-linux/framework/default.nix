664f694ce60d8e8a49b05ab9878e5f48e8e3aa41
Jacob Ranson <code@jacobranson.dev>
2024-05-23 22:58:52 -0400

    Persist secureboot

--- systems/x86_64-linux/framework/default.nix
+++ systems/x86_64-linux/framework/default.nix
@@ -114,6 +114,7 @@ in {
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
+      "/etc/secureboot"
       "/etc/NetworkManager/system-connections"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
